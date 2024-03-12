8afe4639b233c969dd4dc1b0d2945e5bca0181dd
fiardiel <rafi.ardiel@ui.ac.id>
2024-03-12 17:48:28 +0700

    [FEAT] added set_player and is_player for characters

--- characters.py
+++ characters.py
@@ -2,5 +2,11 @@ class Character:
     def __init__(self) -> None:
         self.room = None
         self.is_alive = True
+        self.char_type = None
+        self.player: int = None
 
+    def set_player(self, player: int) -> None:
+        self.player = player
 
+    def is_player(self) -> bool:
+        return self.player is not None
